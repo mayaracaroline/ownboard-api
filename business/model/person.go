277cb67ed8063e1042cb0e0b570b9bc88059f127
Mayara Caroline <mayara.caroline@mercadolivre.com>
2021-11-04 17:30:02 -0300

    Create json presentation of model.Person

--- business/model/person.go
+++ business/model/person.go
@@ -4,26 +4,24 @@ import "time"
 
 type (
 	Person struct {
-		Name       string
-		LastName   string
-		DateOfBorn time.Time
-		Document   string
-		Tipo       string
+		Name       string    `json:"name"`
+		LastName   string    `json:"lastName"`
+		DateOfBorn time.Time `json:"dateOfBorn"`
+		Document   string    `json:"document"`
+		Tipo       string    `json:"tipo"`
 	}
 )
 
 func NewPerson(name string,
 	lastName string,
 	dateOfBorn time.Time,
-	document string) *Person {
+	document string,
+	tipo string) *Person {
 	return &Person{
 		Name:       name,
 		LastName:   lastName,
 		DateOfBorn: dateOfBorn,
 		Document:   document,
+		Tipo:       tipo,
 	}
-}
-
-func (person *Person) setName(name string) {
-	person.Name = name
 }
