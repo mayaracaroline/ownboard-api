636b947a387d7e714574b6620758501d51899cf2
Mayara Caroline <mayara.caroline@mercadolivre.com>
2021-11-11 17:24:29 -0300

    Changes method return to error in repository

--- repositories/repository.go
+++ repositories/repository.go
@@ -4,9 +4,9 @@ import "github.com/mercadolibre/api/business/model"
 
 type Repository interface {
 	Save(person model.Person)
-	Update(person model.Person) bool
+	Update(person model.Person) error
 	FindAll() []model.Person
-	FindByDocument(id string) (bool, model.Person)
+	FindByDocument(id string) (model.Person, error)
 	checkForExistingPerson(id string) bool
 	DeleteByDocument(id string)
 	DeleteAll()
